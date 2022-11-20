775baf4a06b30bc4c6b356490add06f23d37c1a2
Wen-Chi Guo <campbellmath@gmail.com>
2022-11-20 23:34:52 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Install
+
+```
+sh dependencies.sh
+```
---
+++ dependencies.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it
+###############################################################################
+npm install markdown-it --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/highlight.js
+###############################################################################
+npm install highlight.js --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/highlightjs-line-numbers.js
+###############################################################################
+npm install highlightjs-line-numbers.js --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-table-of-contents
+###############################################################################
+
+npm install markdown-it-anchor --save
+npm install markdown-it-table-of-contents --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-sup
+###############################################################################
+npm install markdown-it-sup --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-sub
+###############################################################################
+npm install markdown-it-sub --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-footnote
+###############################################################################
+npm install markdown-it-footnote --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-emoji
+###############################################################################
+npm install markdown-it-emoji --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-task-lists
+###############################################################################
+npm install markdown-it-task-lists --save
+
+###############################################################################
+# ref to https://www.npmjs.com/package/markdown-it-collapsible
+###############################################################################
+npm install markdown-it-collapsible --save
